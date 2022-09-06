18b45ecc08bf647096b50ce177d5863493278e17
lehoangnam1410 <94966143+lehoangnam1410@users.noreply.github.com>
2022-09-06 15:09:39 +0700

    upload

---
+++ light_buld.js
@@ -0,0 +1,13 @@
+function myfunction(){
+    var x=true;
+    if(x){
+    document.getElementById('main').src="../light_buld_close.png"
+x=false;
+}
+else{
+    document.getElementById('main').src="../light_buld_open.png"
+    x=true;
+}
+
+
+}
